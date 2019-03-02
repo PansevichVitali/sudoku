8626bd61aacedc304be45684715cdea5b997ffc8
PansevichVitali <egyptiain94@gmail.com>
2019-03-03 01:13:28 +0300

    feat: add zeroElementPosition Array

--- src/index.js
+++ src/index.js
@@ -1,50 +1,79 @@
 module.exports = function solveSudoku(matrix) {
   const matrixSudoku = matrix;
-  let boolMatchChecking = true;
-
-  for (let row = 0; row < 8; row++) {
-    for(let col = 0; col < 8; col++) {
+  let qttZeros = 0;
+  for (let row = 0; row < 9; row++) {
+    for(let col = 0; col < 9; col++) {
       if(matrix[row][col] === 0) {
-        matrixSudoku[row][col]++;      
-        for(let k = 1; k < 9; k++) {
-          if(boolMatchChecking === findInRows(row,col, matrixSudoku[row][col]) 
-          && boolMatchChecking === findInColumns(row,col, matrixSudoku[row][col])
-          && boolMatchChecking === findInSquare(row,col, matrixSudoku[row][col]))
-            break;
-          else  {
-            matrixSudoku[row][col]++;
-            continue;
-          }
-        }
+        qttZeros++;
+      }
+    }
+  }
+  const zeroElementPosition = new Array(qttZeros);
+  for(let i = 0; i < zeroElementPosition.length; i++) {
+    zeroElementPosition[i] = new Array(2);
+  }
+  for (let row = 0, i = 0; row < 9; row++) {
+    for(let col = 0; col < 9; col++) {
+      if(matrix[row][col] === 0) {
+        zeroElementPosition[i][0] = row;
+        zeroElementPosition[i][1] = col;
+        i++;
+      }
+    }
+  }
+  let row, col;
+  for (let i = 0; i < qttZeros; i++) {
+    row = zeroElementPosition[i][0];
+    col = zeroElementPosition[i][1];
+    matrixSudoku[row][col]++;
+    if(matrixSudoku[row][col] > 9) {
+      matrixSudoku[row][col] = 0;
+      i -= 2;
+      continue;
+    }
+    for(let k = matrixSudoku[row][col]; k < 10; k++) {
+      if(findInRows(row, col, matrixSudoku[row][col]) 
+      && findInColumns(row, col, matrixSudoku[row][col])
+      && findInSquare(row, col, matrixSudoku[row][col]))
+        break;
+      else  matrixSudoku[row][col] = k;
+      if((matrixSudoku[row][col] === 9 && !findInRows(row, col, matrixSudoku[row][col]))
+      || (matrixSudoku[row][col] === 9 && !findInColumns(row, col, matrixSudoku[row][col]))
+      || (matrixSudoku[row][col] === 9 && !findInSquare(row, col, matrixSudoku[row][col]))) {
+        matrixSudoku[row][col] = 0;
+        i -= 2;
+        break;
       }
     }
   }
 
  function findInRows(row,col, valueInMatrix) {
-    for(let i = 0; i < 8; i++) {
-      if(matrix[row][i] === valueInMatrix && i !== col)
+    for(let i = 0; i < 9; i++) {
+      if(matrixSudoku[row][i] === valueInMatrix && i !== col)
         return false;
     }
+    
     return true;
   }
 
   function findInColumns(row,col, valueInMatrix) {
-    for(let i = 0; i < 8; i++) {
-      if(matrix[i][col] === valueInMatrix && i !== row)
+    for(let i = 0; i < 9; i++) {
+      if(matrixSudoku[i][col] === valueInMatrix && i !== row)
         return false;
     }
+    
     return true;
   }
 
   function findInSquare(row,col, valueInMatrix) {
-    for(let i = Math.floor((row)/3)*3; i < Math.ceil((row)/3)*3; i++) {
+    for(let i = Math.floor((row)/3)*3; i < (Math.ceil((row)/3)*3); i++) {
       for(let j = Math.floor((col)/3)*3; j < Math.ceil((col)/3)*3; j++){
-        if(matrix[i][j] === valueInMatrix && i !== row && j !== col)
+        if(matrixSudoku[i][j] === valueInMatrix && i !== row && j !== col)
           return false;
       }
     }
     return true;
   }
-  
+
   return matrixSudoku;
 }
